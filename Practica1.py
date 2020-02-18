b988670cf927cd314e8ee9a47426803abadcc4a8
David251097 <61199557+David251097@users.noreply.github.com>
2020-02-18 08:14:17 -0800

    Primer practica con phyton
    Esta es un primer acercamiento a phyton

---
+++ Practica1.py
@@ -0,0 +1,15 @@
+nombre=input("Como te llamas?")
+print("Hola "+nombre)
+
+
+edad=input("Cuantos años tienes?")
+x = int(edad)
+if x < 18:
+  print("Eres menor de edad")
+  if x > 18:
+  
+   print("Eres mayor de edad")
+   
+   print("Buen Dia!!")
+  
+  
