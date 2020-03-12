3d93655d2a78e0912ad66f329d8314a6cfb1eedf
David251097 <61199557+David251097@users.noreply.github.com>
2020-03-12 09:03:38 -0700

    practica8
    Suma de dos numeros

---
+++ main.py
@@ -0,0 +1,8 @@
+#El siguiente algoritmo resuelve el problema para relaizar la suma de dos numeros decimales
+a=0
+b=0
+c=0
+a=float(input("Ingrese el primer numero:"))
+b=float(input("Ingrese el segundo numero:"))
+c=a+b
+print("la suma es:",c)
